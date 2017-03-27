beca70c2d126e54ead2c316aad9bdcb2a2e06838
haruiz <henry22-12@hotmail.com>
2017-03-27 08:19:50 +0000

    some changes

--- Robot.js
+++ Robot.js
@@ -12,15 +12,14 @@ class Robot{
   }
   //.wav file as binary, it comming from web interface
   //we could be more strict and put all the code inside a try/ catch block, we are just
-<<<<<<< HEAD
+
   //assuming that everything'll be perfect :),  is just a robot
   async speechToText(audioAsBinary){    
     var response = await stt(this.config.BING_SPEECH_API_KEY, audioAsBinary);  return response.data.results;
-=======
+  }
   //assuming that everything'll be perfect :), is just a robot
   async speechToText(audioAsBinary){
     var response = await stt(audioAsBinary);  return response.data.results;
->>>>>>> e37ec691e531fcf5c9dd9d69c08311dfe3ce7861
   }
 }
 
